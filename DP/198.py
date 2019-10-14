eab602e6cc155eab5965919aedfad9aecb597bd8
junyang10734 <junyang2019@outlook.com>
2019-10-14 09:53:34 -0400

    add dp/198.py and dp/53.py

---
+++ DP/198.py
@@ -0,0 +1,43 @@
+# 动态规划
+# dp[i]表示rob第i个房子的最大收益
+# dp[i] = max(nums[i]+dp[i-2],dp[i-1])
+# 注意dp[0]和dp[1]的初始化赋值
+
+# run tims: O(n)  space: O(n)
+class Solution1:
+    def rob(self, nums: List[int]) -> int:
+        account = len(nums)
+        if account == 0 :
+            return 0
+        elif account == 1 :
+            return nums[0]
+        elif account == 2 :
+            return max(nums)
+        else:
+            dp = [0] * account
+            dp[0] = nums[0]
+            dp[1] = nums[1] if nums[1] > dp[0] else dp[0]
+            for i in range(2,account):
+                dp[i] = max(nums[i]+dp[i-2],dp[i-1])
+            return max(dp)
+
+
+# run tims: O(n)  space: O(1)
+class Solution2:
+    def rob(self, nums: List[int]) -> int:
+        account = len(nums)
+        if account == 0 :
+            return 0
+        elif account == 1 :
+            return nums[0]
+        elif account == 2 :
+            return max(nums)
+        else:
+            first = nums[0]
+            second = nums[1] if nums[1] > first else first
+            maxsum = second
+            for i in range(2,account):
+                maxsum = max(nums[i]+first,second)
+                first = second
+                second = maxsum
+            return maxsum
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # leetcode-python
 
 ### data-structure（基本数据结构的实现）
+
 ### array（数组）
+
 ### DaC（动态规划）
-* 169 Majority Element [easy] 
 * 53 Maximum Subarray [easy]
+* 169 Majority Element [easy] 
+
 ### DP（动态规划）
-* 121 Best Time to Buy and Sell Stock [easy]
+* 53 Maximum Subarray [easy]
 * 70 Climbing Stairs [easy]
+* 121 Best Time to Buy and Sell Stock [easy]
+* 198 House Robber [easy]
